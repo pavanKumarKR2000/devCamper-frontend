ff8e697bb88e9c986e1a2b670230049103f5424f
Pavankumar K R <pavan123456pop@gmail.com>
2024-12-17 20:25:39 +0530

    add auth folder

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,12 @@
+import RegisterForm from "@/components/auth/RegisterForm";
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <RegisterForm />
+    </main>
+  );
+};
+
+export default page;
