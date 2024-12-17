06a6e531b9cae7697a0448fa692110294c6c0f29
Pavankumar K R <pavan123456pop@gmail.com>
2024-12-17 20:26:05 +0530

    add home folder

---
+++ src/app/home/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useLogout } from "@/api/auth";
+import { useGetBootcamps } from "@/api/bootcamp";
+import BootcampCard from "@/components/bootcamp/BootcampCard";
+import { Bootcamp } from "@/types/bootcamp";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+
+const page = () => {
+  const { data } = useGetBootcamps();
+  const bootcampData = data?.data as Bootcamp[] | undefined;
+
+  return (
+    <main>
+      <div className="container mx-auto space-y-6 py-10">
+        <h2 className="italic font-medium text-2xl dark:text-white">
+          Bootcamps
+        </h2>
+        <div className="grid grid-cols-4 gap-4">
+          {bootcampData?.map((item) => (
+            <Link key={item.id} href="">
+              <BootcampCard {...item} />
+            </Link>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default page;
