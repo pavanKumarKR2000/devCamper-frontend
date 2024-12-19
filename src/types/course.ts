8cb67f2d1f34f6785ce5e6f8e150c499e2975430
Pavankumar K R <pavan123456pop@gmail.com>
2024-12-19 20:15:38 +0530

    add course types

---
+++ src/types/course.ts
@@ -0,0 +1,12 @@
+export interface Course {
+    _id: string
+    title: string
+    description: string
+    weeks: string
+    tuition: number
+    minimumSkill: string
+    scholarshipAvailable: boolean
+    bootcamp: string
+    createdAt: string
+    __v: number
+  }
