cdb0fe523ccb235989f60f7dbb4b88f53a432c3e
Pavankumar K R <pavan123456pop@gmail.com>
2024-12-21 22:06:45 +0530

    modify middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,42 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getMe } from "./api/auth";
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   const protectedRoutes = ["/home"];
+  const publisherProtectedRoutes = ["/createBootcamp"];
   const authRoutes = ["/auth/login", "/auth/register"];
 
   const authToken = req.cookies.get("token")?.value;
+  let user;
+
+  try {
+    user = await getMe(authToken);
+    user = user?.data;
+  } catch (err) {}
 
   if (pathname === "/") {
     return NextResponse.redirect(new URL("/home", req.url));
   }
 
+  if (
+    publisherProtectedRoutes.includes(pathname) &&
+    user.role !== "publisher" &&
+    user.role !== "admin"
+  ) {
+    console.log("user", user);
+    return NextResponse.redirect(new URL("/home", req.url));
+  }
+
   if (protectedRoutes.includes(pathname)) {
-    if (!authToken && authToken !== "none") {
+    if (!user) {
       const loginUrl = req.nextUrl.clone();
       loginUrl.pathname = "/auth/login";
       return NextResponse.redirect(loginUrl);
     }
   } else if (authRoutes.includes(pathname)) {
-    if (authToken && authToken !== "none") {
+    if (user) {
       const loginUrl = req.nextUrl.clone();
       loginUrl.pathname = "/home";
       return NextResponse.redirect(loginUrl);
@@ -30,5 +47,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/auth/login", "/auth/register", "/home", "/"],
+  matcher: ["/auth/login", "/auth/register", "/home", "/", "/createBootcamp"],
 };
